d98b6290badab297881363dcd155cba2a38d2880
Vandle879 <Vandle879@hotmail.com>
2019-02-15 18:23:43 -0500

    added default command to Drivetrain

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -11,6 +11,7 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.commands.ElmCityDrive;
 
 /**
  * Add your docs here.
@@ -49,6 +50,6 @@ public class Drivetrain extends Subsystem {
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
-    // setDefaultCommand(new ElmCityDrive());
+     setDefaultCommand(new ElmCityDrive());
   }
 }
