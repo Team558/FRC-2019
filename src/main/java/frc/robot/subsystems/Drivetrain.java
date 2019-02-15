29d0723106d288fa90d9191041cbcb6874c07872
Vandle879 <Vandle879@hotmail.com>
2019-02-15 18:17:53 -0500

    Fixing Commands

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -49,6 +49,6 @@ public class Drivetrain extends Subsystem {
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
-     setDefaultCommand(new ElmCityDrive());
+    // setDefaultCommand(new ElmCityDrive());
   }
 }
