ed7177656201998ce5b36fc9d6541c96ccc30779
m4nik1 <maniksoomro@gmail>
2019-03-03 13:10:41 -0500

    RoboMap changes

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -13,8 +13,8 @@ public class RobotMap {
     public static int throttleReverseAxis = 2; 
     public static int quickTurnButton = 2;
     public static int turnAxis = 0;
-    public static double quickturnSensitivity = .80;
-	public static double normalTurnSensitivity = .75;
+    public static double quickturnSensitivity = .69;
+	public static double normalTurnSensitivity = .65;
 	
 
 }
